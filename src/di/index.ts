1f2700ee4879f31d9baa1c8b44a4ed5fb420cba4
Søren Peter Andersen <spa@kamstrup.com>
2025-04-22 22:51:23 +0200

    expose di registry

--- src/di/index.ts
+++ src/di/index.ts
@@ -1,2 +1,8 @@
+export * from "./domain/di-container-key.type";
+export * from "./domain/di-container-map.type";
+export * from "./domain/di-dependency-map.type";
+
 export * from "./application/di-container.interface";
+
 export * from "./infrastructure/di-container";
+export * from "./infrastructure/di-container-registry";
