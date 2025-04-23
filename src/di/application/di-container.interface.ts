bc6d644ff437060d770e3cb372594813d15e8d2c
Søren Peter Andersen <spa@kamstrup.com>
2025-04-23 12:43:08 +0200

    lazy DI registration support

--- src/di/application/di-container.interface.ts
+++ src/di/application/di-container.interface.ts
@@ -1,6 +1,6 @@
 import { DIDependencyMap } from "../domain/di-dependency-map.type";
 
 export interface IDIContainer<M extends DIDependencyMap> {
-  register<K extends keyof M>(key: K, service: M[K]): void;
+  register<K extends keyof M>(key: K, input: () => M[K] | M[K]): void;
   get<K extends keyof M>(key: K): M[K];
 }
--- src/di/infrastructure/di-container.ts
+++ src/di/infrastructure/di-container.ts
@@ -1,25 +1,43 @@
 import { IDIContainer } from "../application/di-container.interface";
 import { DIDependencyMap } from "../domain/di-dependency-map.type";
 
-export class DIContainer<M extends DIDependencyMap> implements IDIContainer<M> {
-  private services: Partial<M> = {};
+type Factory<T> = () => T;
 
-  register<K extends keyof M>(key: K, service: M[K]): void {
-    if (this.services[key]) {
+export class DIContainer<M extends DIDependencyMap> implements IDIContainer<M> {
+  private serviceFactories: Partial<{ [K in keyof M]: Factory<M[K]> }> = {};
+  private serviceInstances: Partial<M> = {};
+
+  register<K extends keyof M>(key: K, service: M[K]): void;
+  register<K extends keyof M>(key: K, factory: Factory<M[K]>): void;
+  register<K extends keyof M>(key: K, input: Factory<M[K]> | M[K]): void {
+    if (this.serviceFactories[key] || this.serviceInstances[key]) {
       throw new Error(
-        `Service with name "${String(key)}" is already registered.`
+        `Service with key "${String(key)}" is already registered.`
       );
     }
-    console.log("Service Registered", key);
 
-    this.services[key] = service;
+    if (typeof input === "function") {
+      this.serviceFactories[key] = input as Factory<M[K]>;
+      console.log("Lazy Service Registered:", key);
+    } else {
+      this.serviceInstances[key] = input;
+      console.log("Eager Service Registered:", key);
+    }
   }
 
   get<K extends keyof M>(key: K): M[K] {
-    const service = this.services[key];
-    if (!service) {
+    if (this.serviceInstances[key]) {
+      return this.serviceInstances[key] as M[K];
+    }
+
+    const factory = this.serviceFactories[key];
+    if (!factory) {
       throw new Error(`Service with key "${String(key)}" is not registered.`);
     }
-    return service;
+
+    const instance = factory();
+    this.serviceInstances[key] = instance;
+
+    return instance;
   }
 }
