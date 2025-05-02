2221073195a8bab5a2891e19c3f0c9e317f7a179
Søren Peter Andersen <spa@kamstrup.com>
2025-05-02 14:47:39 +0200

    WIP event-bus

---
+++ src/domain/common/error-region.ts
@@ -0,0 +1,1 @@
+export type ErrorRegion = "Component" | "Route" | "Layout";
---
+++ src/presentation/common/error-boundary.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { ErrorRegion } from "src/domain/common/error-region";
+
+type Props = {
+  children: React.ReactNode;
+  onError?: (
+    error: Error,
+    errorInfo: React.ErrorInfo,
+    context?: { region: ErrorRegion; name?: string }
+  ) => void;
+  fallback?: React.ReactNode;
+  region?: ErrorRegion;
+  name?: string;
+};
+
+type State = { hasError: boolean; error?: Error };
+
+export class ErrorBoundary extends React.Component<Props, State> {
+  state: State = { hasError: false };
+
+  static getDerivedStateFromError(error: Error): State {
+    return { hasError: true, error };
+  }
+
+  componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
+    this.props.onError &&
+      this.props.onError(error, errorInfo, {
+        name: this.props.name,
+        region: this.props.region || "Component",
+      });
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <>{this.props.fallback || this.renderDefaultError()}</>;
+    }
+
+    return this.props.children;
+  }
+
+  renderDefaultError() {
+    return (
+      <>
+        <div>Something went wrong. Please refresh or try again.</div>
+      </>
+    );
+  }
+}
